a7ebfe131ac040e5fa53abee3f26375ea7a021a0
VietTran8 <cubon882003@gmail.com>
2023-10-28 00:34:40 +0700

    Done login layout

--- app/src/main/java/com/main/travelApp/MainActivity.java
+++ app/src/main/java/com/main/travelApp/MainActivity.java
@@ -3,12 +3,25 @@ package com.main.travelApp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.UnderlineSpan;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView txtForgotPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+        init();
+    }
+
+    private void init(){
+        String forgotPassword = "Quên mật khẩu";
+        txtForgotPassword = findViewById(R.id.txtForgotPassword);
+
+        SpannableString mSpannableString = new SpannableString(forgotPassword);
+        mSpannableString.setSpan(new UnderlineSpan(), 0, forgotPassword.length(), 0);
+        txtForgotPassword.setText(mSpannableString);
     }
 }
