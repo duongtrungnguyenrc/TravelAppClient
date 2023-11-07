5a3de8b17fa6eb43980234df3af012e13a0ccb8c
Đường Trung Nguyên <duongtrungnguyenrc@gmail.com>
2023-11-07 14:31:20 +0700

    restructure using Lombok data library

--- app/src/main/java/com/main/travelApp/models/User.java
+++ app/src/main/java/com/main/travelApp/models/User.java
@@ -1,5 +1,8 @@
 package com.main.travelApp.models;
 
+import lombok.Data;
+
+@Data
 public class User {
     private String id;
     private String fullName;
@@ -8,70 +11,4 @@ public class User {
     private String password;
     private String phone;
     private boolean active;
-
-    public User(String id, String fullName, String email, String address, String password, String phone, boolean active) {
-        this.id = id;
-        this.fullName = fullName;
-        this.email = email;
-        this.address = address;
-        this.password = password;
-        this.phone = phone;
-        this.active = active;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public boolean isActive() {
-        return active;
-    }
-
-    public void setActive(boolean active) {
-        this.active = active;
-    }
 }
