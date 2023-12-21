8963993bc8b6832990415d0cd23d3450431a4731
VietTran8 <cubon882003@gmail.com>
2023-12-21 09:43:30 +0700

    Fixed some stuffs

--- app/src/main/java/com/main/travelApp/ui/fragments/ProfileFragment.java
+++ app/src/main/java/com/main/travelApp/ui/fragments/ProfileFragment.java
@@ -13,11 +13,16 @@ import androidx.lifecycle.ViewModelProvider;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.main.travelApp.R;
 import com.main.travelApp.databinding.FragmentProfileBinding;
 import com.main.travelApp.services.auth.AuthManager;
 import com.main.travelApp.ui.activities.LoginActivity;
+import com.main.travelApp.ui.components.BottomSheet;
+import com.main.travelApp.ui.components.MyDialog;
 import com.main.travelApp.utils.SharedPreferenceKeys;
 import com.main.travelApp.viewmodels.ProfileViewModel;
 import com.main.travelApp.viewmodels.factories.ProfileViewModelFactory;
@@ -27,6 +32,7 @@ public class ProfileFragment extends Fragment implements View.OnClickListener {
     private FragmentProfileBinding profileBinding;
     private ProfileViewModel viewModel;
     private SharedPreferences sharedPreferences;
+    private BottomSheet userInfoBottomSheet;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -40,6 +46,8 @@ public class ProfileFragment extends Fragment implements View.OnClickListener {
     }
 
     private void init(){
+        userInfoBottomSheet = new BottomSheet(getContext(), getLayoutInflater(), R.layout.fragment_profile, "Hồ sơ cá nhân");
+
         profileBinding.txtUserName.setText(viewModel.getCurrentUser().getFullName());
         if(!viewModel.getCurrentUser().getAvatar().isEmpty() && viewModel.getCurrentUser().getAvatar() != null)
             Picasso.get()
@@ -48,8 +56,29 @@ public class ProfileFragment extends Fragment implements View.OnClickListener {
                 .error(R.color.light_gray)
                 .into(profileBinding.userAvatar);
         profileBinding.btnSignOut.setOnClickListener(this);
+        profileBinding.btnPersonalInfo.setOnClickListener(this);
+        profileBinding.btnLoginHistory.setOnClickListener(this);
+        profileBinding.btnOrderHistory.setOnClickListener(this);
+        profileBinding.btnPasswordAndSecure.setOnClickListener(this);
+        profileBinding.btnPersonalInfo1.setOnClickListener(this);
+        profileBinding.btnPushNotification.setOnClickListener(this);
+        profileBinding.btnTermsAndServices.setOnClickListener(this);
     }
 
+    private void showUserInfoBottomSheet(){
+        userInfoBottomSheet.show((dialog, contentView) -> {
+            contentView.findViewById(R.id.btnPersonalInfo).setOnClickListener(view -> {
+                MyDialog myDialog = new MyDialog(getContext(), getLayoutInflater(), R.layout.fragment_enter_code_dialog, (d, v) -> {
+                    EditText edt = v.findViewById(R.id.edtConfirmCode);
+                    Button btn = v.findViewById(R.id.btnSend);
+                    btn.setOnClickListener(mView -> {
+
+                        });
+                    });
+                myDialog.show(getParentFragmentManager(), "DIALOG");
+            });
+        });
+    }
     @Override
     public void onClick(View view) {
         if(view == profileBinding.btnSignOut){
@@ -58,6 +87,8 @@ public class ProfileFragment extends Fragment implements View.OnClickListener {
             Intent intent = new Intent(requireActivity(), LoginActivity.class);
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
             startActivity(intent);
+        }else if(view == profileBinding.btnPersonalInfo){
+            showUserInfoBottomSheet();
         }
     }
 }
